6345ddcce887d044b44c4dbb0d067a55ee44006f
Jeferson Santos Mota <jefersonsantos.jsm@gmail.com>
2022-12-29 15:51:28 -0400

    Delete old code

--- CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-## 0.0.4
-
-* Added autoStartSettingIsAvailable function
-
-## 0.0.3
-
-* Added null safety
--- example/android/app/src/main/kotlin/com/example/android_autostart_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.android_autostart_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
